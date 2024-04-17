238e127ab66e791effe29d5c77df24c0d21fcf0f
Kiran Viswanath Balusu <kiranvb.dev@gmail.com>
2024-04-17 00:09:25 -0500

    Display, Images on Submit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
+import ImageGrid from "./components/ImageGrid.jsx";
 
 function App() {
+
+    const [images, setData] = useState([]);
+
     return (
-        <div>
-            <Navbar/>
+        <div className="flex flex-col">
+            <Navbar setData={setData}/>
+            {
+                images.length > 0 &&
+                <ImageGrid images={images}/>
+            }
         </div>
     )
 }
